a574e525ee60f301ff81257e0a859cf7116f0db3
hnty545 <hnty545@foxmail.com>
2019-02-07 09:29:48 +0800

    vant-weapp升级

--- static/vant-weapp/collapse-item/index.js
+++ static/vant-weapp/collapse-item/index.js
@@ -1,6 +1,6 @@
 import { VantComponent } from '../common/component';
 VantComponent({
-  classes: ['content-class'],
+  classes: ['title-class', 'content-class'],
   relation: {
     name: 'collapse',
     type: 'ancestor',
@@ -41,17 +41,15 @@ VantComponent({
       }
 
       var _this$parent$data = this.parent.data,
-        value = _this$parent$data.value,
-        accordion = _this$parent$data.accordion,
-        items = _this$parent$data.items;
+          value = _this$parent$data.value,
+          accordion = _this$parent$data.accordion,
+          items = _this$parent$data.items;
       var name = this.data.name;
       var index = items.indexOf(this);
       var currentName = name == null ? index : name;
-      var expanded = accordion
-        ? value === currentName
-        : value.some(function(name) {
-            return name === currentName;
-          });
+      var expanded = accordion ? value === currentName : value.some(function (name) {
+        return name === currentName;
+      });
 
       if (expanded !== this.data.expanded) {
         this.updateStyle(expanded);
@@ -64,29 +62,23 @@ VantComponent({
     updateStyle: function updateStyle(expanded) {
       var _this = this;
 
-      this.getRect('.van-collapse-item__content').then(function(res) {
-        var animationData = _this.animation
-          .height(expanded ? res.height : 0)
-          .step()
-          .export();
+      this.getRect('.van-collapse-item__content').then(function (res) {
+        var animationData = _this.animation.height(expanded ? res.height : 0).step().export();
 
         if (expanded) {
           _this.set({
             animationData: animationData
           });
         } else {
-          _this.set(
-            {
-              contentHeight: res.height + 'px'
-            },
-            function() {
-              setTimeout(function() {
-                _this.set({
-                  animationData: animationData
-                });
-              }, 20);
-            }
-          );
+          _this.set({
+            contentHeight: res.height + 'px'
+          }, function () {
+            setTimeout(function () {
+              _this.set({
+                animationData: animationData
+              });
+            }, 20);
+          });
         }
       });
     },
@@ -96,8 +88,8 @@ VantComponent({
       }
 
       var _this$data = this.data,
-        name = _this$data.name,
-        expanded = _this$data.expanded;
+          name = _this$data.name,
+          expanded = _this$data.expanded;
       var index = this.parent.data.items.indexOf(this);
       var currentName = name == null ? index : name;
       this.parent.switch(currentName, !expanded);
